d2577b2e7912b4e26bccee1c84dc8e4c4073f7b9
Tanay Patidar <patidar3tanay@gmail.com>
2022-03-24 23:42:30 +0530

    3/23/2022

--- lessthan_1300/318A.cpp
+++ lessthan_1300/318A.cpp
@@ -25,3 +25,16 @@ int main() {
     cout<<val;
     return 0;
 }
+
+
+// efficient approach
+    // long long n, k;
+    // cin >> n >> k;
+    // if (k <= (n + 1) / 2)
+    // {
+    //     cout << k * 2 - 1 << endl;
+    // }
+    // else
+    // {
+    //     cout << (k - (n + 1) / 2) * 2 << endl;
+    // }
